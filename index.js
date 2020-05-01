ad25bb18f6c69b5c0ca5dded89a05d29d0ecccd0
kiennguyen131 <sayohyeah17@gmai.com>
2020-05-01 12:11:10 +0700

    dome demo bai 7

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var low =require('lowdb'); 
-var FileSync =  require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
+var shortid = require('shortid');
+var bodyParser = require("body-parser");
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
 
-db =low(adapter);
+
+var adapter = new FileSync('db.json')
+var db = low(adapter)
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults( { toDoList: []} )
-  .write();
+db.defaults({ toDoList: [] })
+  .write()
 
-var port = 3000;
+app.set("view engine", "pug");
+app.set("views", "./views");
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-app.get('/', function (req, res) {
-	res.end('hello world');
-})
-
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.end("hello world");
+});
 
 app.get("/todos", (req, res) => {
-	res.render('index',{
-		toDoList: db.get('toDoList').value()
+  res.render("index", {
+    toDoList: db.get('toDoList').value()
+  });
+});
+
+app.get("/create", function(req, res) {
+  res.render("create");
+});
+
+app.get('/toDoList/:id', function(req, res) {
+	var id =req.params.id;
+
+	var toDo = db.get('toDoList').find({id: id}).value();
+
+	res.render('view', {
+		toDo: toDo
 	});
 });
 
-app.get("/create",function(req, res){
-	res.render('create');
-})
-
-app.post("/views/create",function(req, res){
-	db.get('toDoList').value().push(req.body.name);
-	res.redirect('/todos');
-})
 
 
+app.post("/views/create", function(req, res) {
+	req.body.id = shortid.generate();
+ 	db.get('toDoList').push(req.body).write();
+ 	res.redirect("/todos");
+});
 
-app.listen(port, function(){
+app.listen(3000, function(){
 	console.log('server listening on port' + 3000);
 });
